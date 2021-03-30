f1b31effbd9adc5c84f957e8fcb18345ecb5dc90
龚成志 <gongcz@koal.com>
2021-03-30 19:38:41 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use crossterm::event::{self, Event, MouseEvent, MouseEventKind};
 use crossterm::{cursor, execute, terminal};
+
 use std::time::{Duration, Instant};
 use std::{io, panic};
+
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+mod app;
+mod draw;
+mod widget;
 
 fn setup_events() {
     let mut stdout = io::stdout();
@@ -21,23 +29,29 @@ fn cleanup_terminal() {
 }
 
 fn setup_panic() {
-    panic::set_hook(Box::new(|_| {
+    panic::set_hook(Box::new(|info| {
+        println!("{:?}", info);
         cleanup_terminal();
     }));
 }
 
 fn main() {
+    let backend = CrosstermBackend::new(io::stdout());
+    let mut terminal = Terminal::new(backend).unwrap();
     setup_panic();
     setup_events();
+
+    let mut app = app::App::new();
     loop {
-        let key_timeout = Duration::from_millis(30);
+        draw::draw(&mut terminal, &app);
+        let key_timeout = Duration::from_millis(100);
         let now = Instant::now();
         if event::poll(key_timeout).unwrap() {
             if let Event::Key(key) = event::read().unwrap() {
-                println!("{:?}", key);
+                // println!("{:?}", key);
                 match key.code {
                     event::KeyCode::Char('q') | event::KeyCode::Char('Q') => break,
-                    _ => {}, 
+                    _ => {}
                 }
             }
         }
