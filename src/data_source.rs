520f6356fd8d88373b05bd4948709ab68c97a2f5
joinhack <joinhack@gmail.com>
2021-06-13 15:30:21 +0800

    add event in thread

--- src/data_source.rs
+++ src/data_source.rs
@@ -1,24 +1,38 @@
-use bytes::Bytes;
-use flate2::read::GzDecoder;
 use futures::stream::StreamExt;
 use futures::SinkExt;
+
 use serde::Serialize;
 use serde_json;
+
 use std::io::Read;
+use std::sync::{Arc, Mutex};
 use std::thread;
+
 use tokio::runtime;
 use websocket_lite::{Message, Opcode, Result};
+use bytes::Bytes;
+use flate2::read::GzDecoder;
+use crossbeam_channel::Sender;
 
-pub struct DataSource {
+
+struct DataSourceInner {
     source: String,
     ping_times: u64,
     is_sub: bool,
 }
 
+pub enum StockData {
+
+}
+
+pub struct DataSource {
+    inner: Arc<Mutex<DataSourceInner>>,
+}
+
 enum SubStatus {
     Disable(u64),
     Enable,
-    InSub,    
+    InSub,
 }
 
 #[derive(Serialize)]
@@ -35,9 +49,9 @@ struct Sub<'a> {
 type AsyncClient =
     websocket_lite::AsyncClient<Box<dyn websocket_lite::AsyncNetworkStream + Unpin + Send + Sync>>;
 
-impl DataSource {
-    pub fn new(source: String) -> DataSource {
-        DataSource {
+impl DataSourceInner {
+    pub fn new(source: String) -> DataSourceInner {
+        DataSourceInner {
             source,
             ping_times: 0,
             is_sub: false,
@@ -79,51 +93,66 @@ impl DataSource {
             false
         }
     }
+}
 
-    pub fn run(&mut self) {
-        let source = self.source.clone();
-        // thread::spawn(move ||{
-        let rt = runtime::Builder::new_current_thread()
-            .enable_io()
-            .build()
-            .unwrap();
-        rt.block_on(async {
-            let builder = websocket_lite::ClientBuilder::new(&source).unwrap();
-            let mut ws_stream = builder.async_connect().await.unwrap();
-            loop {
-                let ws_msg: Option<Result<Message>> = ws_stream.next().await;
-                if ws_msg.is_none() {
-                    return;
-                }
-                let ws_msg = ws_msg.unwrap();
-                if let Err(e) = ws_msg {
-                    eprintln!("message recv error, detail:{}", e);
-                    return;
-                }
-                let msg = ws_msg.unwrap().into_data();
-                let mut gz_data = GzDecoder::new(&msg[..]);
-                let mut data = String::new();
-                if let Err(e) = gz_data.read_to_string(&mut data) {
-                    eprintln!("message gzip decode error, detail {}", e);
-                    return;
-                }
-                let sval = serde_json::from_str::<serde_json::Value>(&data);
-                if let Err(e) = sval {
-                    eprintln!("parse json error, detail {}", e);
-                    return;
-                }
-                println!("{:?}", sval);
-                if let serde_json::Value::Object(json) = sval.unwrap() {
-                    if json.get("ping").is_some() {
-                        self.process_ping(&json, &mut ws_stream).await;
+impl DataSource {
+    fn clone_soruce(&self) -> String {
+        let guard = (*self.inner).lock().unwrap();
+        (*guard).source.clone()
+    }
+
+    pub fn new(source: String) -> DataSource {
+        DataSource {
+            inner: Arc::new(Mutex::new(DataSourceInner::new(source))),
+        }
+    }
+    pub fn run(&mut self, sender: Sender<StockData>) {
+        let mut inner = self.inner.clone();
+        let source = self.clone_soruce();
+        thread::spawn(move || {
+            let rt = runtime::Builder::new_current_thread()
+                .enable_io()
+                .build()
+                .unwrap();
+            rt.block_on(async {
+                let builder = websocket_lite::ClientBuilder::new(&source).unwrap();
+                let mut ws_stream = builder.async_connect().await.unwrap();
+                loop {
+                    let ws_msg: Option<Result<Message>> = ws_stream.next().await;
+                    if ws_msg.is_none() {
+                        return;
+                    }
+                    let ws_msg = ws_msg.unwrap();
+                    if let Err(e) = ws_msg {
+                        eprintln!("message recv error, detail:{}", e);
+                        return;
+                    }
+                    let msg = ws_msg.unwrap().into_data();
+                    let mut gz_data = GzDecoder::new(&msg[..]);
+                    let mut data = String::new();
+                    if let Err(e) = gz_data.read_to_string(&mut data) {
+                        eprintln!("message gzip decode error, detail {}", e);
+                        return;
+                    }
+                    let sval = serde_json::from_str::<serde_json::Value>(&data);
+                    if let Err(e) = sval {
+                        eprintln!("parse json error, detail {}", e);
+                        return;
+                    }
+                    println!("{:?}", sval);
+                    let mut inner_guard = (*inner).lock().unwrap();
+                    if let serde_json::Value::Object(json) = sval.unwrap() {
+                        if json.get("ping").is_some() {
+                            (*inner_guard).process_ping(&json, &mut ws_stream).await;
+                        }
+                    }
+                    if (*inner_guard).enable_sub() {
+                        (*inner_guard)
+                            .sub("market.ethusdt.kline.1min", "id1", &mut ws_stream)
+                            .await;
                     }
                 }
-
-                if self.enable_sub() {
-                    self.sub("market.ethusdt.kline.1min", "id1", &mut ws_stream).await;
-                }
-            }
+            });
         });
-        // });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use crossterm::event::{self, Event, MouseEvent, MouseEventKind};
+use crossterm::event::{self, Event, KeyEvent, MouseEvent, MouseEventKind};
 use crossterm::{cursor, execute, terminal};
 
 use std::time::{Duration, Instant};
-use std::{io, panic};
+use std::{io, panic, thread};
 
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
+
+use crossbeam_channel::{bounded, Sender, select};
 
 mod app;
 mod data_source;
@@ -13,12 +15,23 @@ mod draw;
 mod theme;
 mod widget;
 
-fn setup_events() {
+fn setup_events(sender: Sender<KeyEvent>) {
     let mut stdout = io::stdout();
     execute!(stdout, cursor::Hide).unwrap();
     execute!(stdout, terminal::EnterAlternateScreen).unwrap();
     execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
     terminal::enable_raw_mode().unwrap();
+
+    thread::spawn(move || {
+        loop {
+            let key_timeout = Duration::from_millis(300);
+            if event::poll(key_timeout).unwrap() {
+                if let Event::Key(key) = event::read().unwrap() {
+                    sender.send(key).unwrap();
+                }
+            }
+        }
+    });
 }
 
 fn cleanup_terminal() {
@@ -40,27 +53,27 @@ fn setup_panic() {
 fn main() {
     let backend = CrosstermBackend::new(io::stdout());
     let mut terminal = Terminal::new(backend).unwrap();
-
+    let (evt_s, evt_r) = bounded(1);
+    let (msg_s, msg_r) = bounded(1);
     let mut ds = data_source::DataSource::new(String::from("wss://api.huobi.pro/ws"));
-    ds.run();
+    ds.run(msg_s);
     setup_panic();
-    setup_events();
+    setup_events(evt_s);
     let mut app = app::App::new();
+    draw::draw(&mut terminal, &app);
     loop {
-        draw::draw(&mut terminal, &app);
-        let key_timeout = Duration::from_millis(100);
-        let now = Instant::now();
-        if event::poll(key_timeout).unwrap() {
-            if let Event::Key(key) = event::read().unwrap() {
-                // println!("{:?}", key);
-                match key.code {
-                    event::KeyCode::Char('q') | event::KeyCode::Char('Q') => break,
-                    _ => {}
+        select! {
+            recv(evt_r) -> msg => {
+                if let Ok(key) = msg {
+                    match key.code {
+                        event::KeyCode::Char('q') | event::KeyCode::Char('Q') => break,
+                        _ => {}
+                    }
                 }
-            }
-        }
-        if now.elapsed() >= key_timeout {
-            //println!("time out!");
+            },
+            recv(msg_r) -> msg => {
+
+            },
         }
     }
     cleanup_terminal();
